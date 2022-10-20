de8ae3ea06347d45284908b731b449f3e77b8e5b
Nahuel Gonzalez <nahuelun59@gmail.com>
2022-10-19 21:31:45 -0300

    objeto

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 //CALCULADORA DE PROMEDIO 
 // si tu promedio es menor a 8 se repetira el ciclo hasta que sea mayor
 
+// se piden los datos del alumno
+function alumno1 (nombre,edad,materia){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.materia = materia;
+}
 
-let nombreApellido = prompt("Ingrese su apellido y nombre");
+const alumno = new alumno1 (
+    this.nombre = prompt("ingrese su nombre"),
+    this.edad = prompt("ingrese su edad"),
+    this.materia = prompt("ingrese la materia")
+);
 let primerTrimestre = parseFloat(prompt("Primer trimestre"));
 let segundoTrimestre = parseFloat(prompt("Segundo trimestre"));
 let final = parseFloat(prompt("Final"));
@@ -10,9 +20,9 @@ let promedio = (primerTrimestre + segundoTrimestre + final) / 3;
 
 function calcularPromedio() {
     if (promedio >= 8) {
-        alert(nombreApellido + " tu promedio es " + promedio + " estas aprobado!");
+        alert(nombre + " tu promedio es " + promedio + " estas aprobado!");
     } else {
-        alert(nombreApellido + " tu promedio es " + promedio + " estas desaprobado");
+        alert(nombre + " tu promedio es " + promedio + " estas desaprobado");
     }
 };
 
@@ -23,7 +33,7 @@ while (true) {
         alert("recibiras tu nota en unos dias")
         break;
     } else {
-        let nombreApellido = parseInt(prompt("ingrese su apellido y nombre"));
+        this.nombre = parseInt(prompt("ingrese su apellido y nombre"));
         let primerTrimestre = prompt("primer trimestre");
         let segundoTrimestre = prompt("segundo trimestre");
         let final = prompt("final");
@@ -32,3 +42,4 @@ while (true) {
     }
 }
 
+console.log(alumno);
